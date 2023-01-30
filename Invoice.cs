2fef335eeb01a2036edca49368a00c813af98a24
akbar <akbar@APC>
2023-01-30 04:24:09 -0800

    Add project files.

---
+++ Invoice.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw9
+{
+    internal class Invoice
+    {
+        string account;
+        string customer;
+        string provider;
+        public Invoice(string account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public decimal CostCalculation(bool needEdv)
+        {
+            
+            if(needEdv)
+            {
+                decimal edv = Price * Quantity * 0.18M;
+                return (Price  * Quantity) + edv; 
+            }
+            else
+            {
+                return Price * Quantity;
+            }
+
+        }
+        
+        
+
+
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace hw9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv = new Invoice("678904", "Alex", "Foxtrot");
+            inv.Article = "USB-hab";
+            inv.Quantity = 6;
+            inv.Price = 30;
+            Console.WriteLine(inv.CostCalculation(true));
+        }
+    }
+}
